bbeac03f7cdd98c1f6311ec1dfec4cce98c52767
matteobrandetti <mat.brandetti@stud.uniroma3.it>
2019-06-15 16:54:33 +0200

    Modificata galleria.

--- ACMESilph/src/main/java/it/uniroma3/siw/controller/VisitatoreController.java
+++ ACMESilph/src/main/java/it/uniroma3/siw/controller/VisitatoreController.java
@@ -1,16 +1,14 @@
 package it.uniroma3.siw.controller;
 
-import java.io.ByteArrayInputStream;
-
 import java.io.IOException;
-import java.io.InputStream;
+import java.time.LocalDate;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import it.uniroma3.siw.model.Album;
 import it.uniroma3.siw.model.Foto;
 import it.uniroma3.siw.model.Funzionario;
+import it.uniroma3.siw.model.RichiestaUtilizzo;
 import it.uniroma3.siw.service.AlbumServices;
 import it.uniroma3.siw.service.FotoServices;
 import it.uniroma3.siw.service.FotografoServices;
@@ -43,6 +42,9 @@ public class VisitatoreController {
 	@Autowired
 	private RichiestaUtilizzoServices richiestaUtilizzoServices;
 	
+	@Autowired
+	private RichiestaUtilizzoValidator richiestaUtilizzoValidator;
+	
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String getIndex() {
@@ -54,7 +56,7 @@ public class VisitatoreController {
 		Funzionario funzionario=this.funzionarioServices.getFunzionarioByEmail(possibileFunzionario.getEmail());
 		if(funzionario!=null && funzionario.checkPassword(possibileFunzionario.getPassword())) {
 				model.addAttribute("funzionarioCorrente", funzionario);
-				return "homeFunzionario.html";
+				return "elencoAttivita.html";
 		}
 		else
 			return "login.html";
@@ -63,6 +65,7 @@ public class VisitatoreController {
 	@RequestMapping(value="/galleriaFoto", method=RequestMethod.GET)
 	public String getGalleriaFoto(Model model) {
 		model.addAttribute("photos", this.fotoServices.getAllFotoAsList());
+		model.addAttribute("richiestaUtilizzo", new RichiestaUtilizzo());
 		return "galleria.html";
 	}
 	
@@ -106,7 +109,23 @@ public class VisitatoreController {
 	
 	
 	@RequestMapping(value="/getGalleria", method=RequestMethod.GET)
-	public String getGalleriaFoto() {
+	public String goBackToGalleriaFoto(Model model) {
+		model.addAttribute("richiestaUtilizzo", new RichiestaUtilizzo());
+
+		return "galleria.html";
+	}
+	
+	
+	@RequestMapping(value="/processaRichiestaUtilizzo", method=RequestMethod.POST)
+	public String processaRichiesta(@ModelAttribute("richiestaUtilizzo") RichiestaUtilizzo richiesta,Model model,  BindingResult result) {
+		this.richiestaUtilizzoValidator.validate(richiesta,result);
+		if (!result.hasErrors()) {
+			richiesta.setData(LocalDate.now());
+			this.richiestaUtilizzoServices.add(richiesta);
+			
+		}
+		model.addAttribute("photos", this.fotoServices.getAllFotoAsList());
+		
 		return "galleria.html";
 	}
 
